a97ef9b869e6039722df0af9a5f98914e03d183e
王俊 <244575182@qq.com>
2018-04-15 23:23:37 +0800

    1.添加弹出菜单

--- app/src/main/java/alien/com/ui/MainActivity.java
+++ app/src/main/java/alien/com/ui/MainActivity.java
@@ -1,9 +1,13 @@
-package alien.com.httputil;
+package alien.com.ui;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.telecom.Call;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,17 +15,46 @@ import android.widget.Toast;
 
 import alien.com.entity.HttpResult;
 import alien.com.entity.UserInfo;
+import alien.com.httputil.HttpUtil;
+import alien.com.httputil.R;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView text;
+    private Toolbar titlebar;
+    private Menu menu;
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        titlebar=findViewById(R.id.titlebar);
+
+        //加载弹出菜单布局
+        titlebar.inflateMenu(R.menu.menu);
+        menu=titlebar.getMenu();
+        //为弹出菜单item设置点击事件
+        menu.findItem(R.id.item1).setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(MainActivity.this, "糟糕，被点中了！", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        //为导航按钮设置点击事件
+        titlebar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, ""+v.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
         text=findViewById(R.id.text);
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
---
+++ app/src/main/java/alien/com/ui/ToolBarActivity.java
@@ -0,0 +1,15 @@
+package alien.com.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alien.com.httputil.R;
+
+public class ToolBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_bar);
+    }
+}
